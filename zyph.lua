52d707dd60df80df97a71163f261393e8bd79448
salthub-master <67442760+salthub-master@users.noreply.github.com>
2020-09-30 21:43:33 +0300

    Update zyph.lua

--- zyph.lua
+++ zyph.lua
@@ -352,11 +352,10 @@ gg.toast('Dont crouch right now.')
 local old = gg.getRanges();
 gg.setRanges(gg.REGION_C_ALLOC)
 gg.searchNumber('1.5', gg.TYPE_FLOAT, false)
-local t = gg.getResults(1500)
-for i, v in ipairs(t) do
-	t[i].value = '2.5'
-	t[i].freeze = true
-	gg.addListItems(t)
+t = gg.getResults(1500)
+t[98].value = '2.5'
+t[98].freeze = true
+t[98].freezeType = gg.FREEZE_NORMAL
 gg.clearResults()
 gg.toast('Fly Hack Activated, now u can couch')
 gg.setRanges(old)
